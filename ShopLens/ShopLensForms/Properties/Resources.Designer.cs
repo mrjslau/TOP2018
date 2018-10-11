38e5ce2bad01a909ab297801b92e94d8dde94841
Edvardas Dlugauskas <edvardasdlugauskas@gmail.com>
2018-10-11 20:01:57 +0300

    Fix leftover naming inconsistencies

--- ShopLens/ShopLensForms/Properties/Resources.Designer.cs
+++ ShopLens/ShopLensForms/Properties/Resources.Designer.cs
@@ -39,7 +39,7 @@ namespace ShopLensApp.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("WindowsFormsApp.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("ShopLensApp.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
