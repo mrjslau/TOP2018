2e065789f5c6ff9abb0e1eb5e15412876e0d2416
marijusiMac <marijuslau@yahoo.com>
2018-11-12 04:12:09 +0200

    Add custom prefs class. Add custom persistence to Cart Activity

---
+++ Mobile/ShopLens/Droid/Source/AppPreferences.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using Android.Content;
+
+namespace ShopLens.Droid.Source
+{
+    public class ActivityPreferences
+    {
+        ISharedPreferences prefs;
+        ISharedPreferencesEditor prefsEditor;
+        Context pContext;
+        List<string> items;
+        int itemCounter = 0;
+        //test
+
+        public ActivityPreferences(Context context, string name)
+        {
+            pContext = context;
+            prefs = pContext.GetSharedPreferences(name, FileCreationMode.Private);
+            prefsEditor = prefs.Edit();
+            foreach (KeyValuePair<string, object> entry in prefs.All)
+            {
+                itemCounter += 1;
+            }
+        }
+
+        public List<string> GetPreferencesToList()
+        {
+            if (items == null)
+            {
+                items = new List<string> { };
+            }
+            foreach (KeyValuePair<string, object> entry in prefs.All)
+            {
+                items.Add(entry.Value.ToString());
+            }
+            return items;
+        }
+
+        public void AddString(string name)
+        {
+            itemCounter += 1;
+            prefsEditor.PutString("item" + itemCounter.ToString(), name);
+            prefsEditor.Apply();
+        }
+    }
+}
---
+++ Mobile/ShopLens/Droid/Source/ShoppingCartActivity.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Android.App;
+using Android.OS;
+using Android.Widget;
+using ShopLens.Droid.Source;
+
+namespace ShopLens.Droid
+{
+    [Activity(Label = "ShoppingCartActivity")]
+    public class ShoppingCartActivity : Activity
+    {
+        readonly string PREFS_NAME = "USER_SHOP_CART";
+
+        EditText addItemEditText;
+        Button addItemButton;
+        ListView listView;
+        ActivityPreferences prefs;
+
+        List<string> items;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.ShoppingCart);
+
+            prefs = new ActivityPreferences(this, PREFS_NAME);
+            items = prefs.GetPreferencesToList();
+
+            listView = FindViewById<ListView>(Resource.Id.ShopCartList);
+            addItemButton = FindViewById<Button>(Resource.Id.ShopCartAddItemButton);
+            addItemEditText = FindViewById<EditText>(Resource.Id.ShopCartAddItemEditText);
+
+            ArrayAdapter<string> listAdapter = 
+                new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleListItemChecked, items);
+            listView.Adapter = listAdapter;
+            listView.ChoiceMode = ChoiceMode.Multiple;
+
+            addItemButton.Click += (sender, e) =>
+            {
+                if (!String.IsNullOrWhiteSpace(addItemEditText.Text)){
+                    listAdapter.Add(addItemEditText.Text);
+                    prefs.AddString(addItemEditText.Text);
+                }
+                listAdapter.NotifyDataSetChanged();
+            };
+        }
+
+    }
+}
