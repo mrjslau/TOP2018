7227e8a4f5deea1096653fd8223548c35dcd415f
MellowJay <marijusjasinskas65@gmail.com>
2018-11-17 12:53:55 +0200

    Removed Recycle() call.

--- Mobile/ShopLens/Droid/Activities/CameraActivity.cs
+++ Mobile/ShopLens/Droid/Activities/CameraActivity.cs
@@ -170,7 +170,6 @@ namespace ShopLens.Droid
             int width = ImgView.Width;
             var image = MediaStore.Images.Media.GetBitmap(ContentResolver, uri);
             image = BitmapHelper.ScaleDown(image, Math.Min(height, width));
-            ImgView.RecycleBitmap();
             ImgView.SetImageBitmap(image);
         }
 
