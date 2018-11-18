e4f6d3b7828033aa2f5274098b917b1120594477
Edvardas Dlugauskas <edvardasdlugauskas@gmail.com>
2018-11-18 21:29:14 +0200

    Merge branch 'DEV' of C:\Users\Edvard\RiderProjects\TOP2018 with conflicts.

---
+++ Mobile/ShopLens/Droid/Helpers/DependencyInjection.cs
@@ -0,0 +1,22 @@
+using ImageRecognitionMobile.Classificators;
+using Unity;
+using Unity.Lifetime;
+
+namespace ShopLens.Droid.Helpers
+{
+    public static class DependencyInjection
+    {
+        public static IUnityContainer Container { get; } = new UnityContainer();
+
+        public static void RegisterInterfaces()
+        {
+            // Singleton-like
+            Container.RegisterType<IDirectoryCreator, ShopLensPictureDirectoryCreator>(
+                new ContainerControlledLifetimeManager());
+
+            // New instance each time resolved
+            Container.RegisterType<IAsyncImageClassificator, WebClassificator>(
+                new ContainerControlledTransientManager());
+        }
+    }
+}
--- Mobile/ShopLens/ImageRecognitionMobile/Classificators/WebClassificator.cs
+++ Mobile/ShopLens/ImageRecognitionMobile/Classificators/WebClassificator.cs
@@ -1,35 +1,33 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using ImageRecognitionMobile.Classificators;
 using ImageRecognitionMobile.Classificators.Helpers;
+using PCLAppConfig;
 
-namespace ImageRecognition.Classificators
+namespace ImageRecognitionMobile.Classificators
 {
     // For more see https://southcentralus.dev.cognitive.microsoft.com/docs/services/450e4ba4d72542e889d93fd7b8e960de/operations/5a6264bc40d86a0ef8b2c290
-    /// <inheritdoc />
+    /// <inheritdoc cref="IAsyncImageClassificator" />
     /// <summary>
     /// This class classifies images by using the customvision.ai API.
     /// </summary>
     public class WebClassificator : IImageClassificator, IAsyncImageClassificator
     {
         /// <inheritdoc/>
-        public Dictionary<string, float> ClassifyImage(byte[] image, string cvProjectId, string cvPredictionKey, string cvRequestUri)
+        public Dictionary<string, float> ClassifyImage(byte[] image)
         {
-            return ClassifyImageAsync(image, cvProjectId, cvPredictionKey, cvRequestUri).GetAwaiter().GetResult();
+            return ClassifyImageAsync(image).GetAwaiter().GetResult();
         }
 
         /// <inheritdoc cref="ClassifyImage" />
         /// <summary>Same as <see cref="ClassifyImage"/> but performed asynchronously.</summary>
-        public async Task<Dictionary<string, float>> ClassifyImageAsync(byte[] image, string cvProjectId, string cvPredictionKey, string cvRequestUri)
+        public async Task<Dictionary<string, float>> ClassifyImageAsync(byte[] image)
         {
-            var projectId = Guid.Parse(cvProjectId);
-            var predictionKey = cvPredictionKey;
-            var uri = cvRequestUri;
+            var predictionKey = ConfigurationManager.AppSettings["cvPredictionKey"];
+            var uri = ConfigurationManager.AppSettings["cvRequestUri"];
 
             // Form request
             var client = new HttpClient();
