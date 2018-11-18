2931f690573f8ec451c211adda6866db65125aa3
MellowJay <marijusjasinskas65@gmail.com>
2018-11-18 12:14:05 +0200

    Implemented Cart and List voice commands, also a String Extension method.

---
+++ Mobile/ShopLens/Droid/Helpers/ProductNameHelper.cs
@@ -0,0 +1,35 @@
+﻿namespace ShopLens.Droid.Helpers
+{
+    public static class ProductNameHelper
+    {
+        public static string GetProductNameFromString(string products)
+        {
+            string[] array = products.Split(' ');
+            string productName = string.Empty;
+
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (i > 0)
+                {
+                    if (i == 1)
+                    {
+                        productName += array[i];
+                    }
+                    else
+                    {
+                        productName += " " + array[i];
+                    }
+                }
+            }
+
+            if (string.IsNullOrEmpty(productName))
+            {
+                return null;
+            }
+            else
+            {
+                return productName;
+            }
+        }
+    }
+}
