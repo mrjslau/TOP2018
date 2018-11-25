3924356fcad2ad67b0671605a509d9039234ee44
Edvardas Dlugauskas <edvardasdlugauskas@gmail.com>
2018-11-25 16:09:01 +0200

    Add OCR. Refactor CameraActivity.cs. Voice Weight of product (if found).
    Names changed according to C# standard naming conventions
    https://docs.microsoft.com/en-us/dotnet/standard/design-guidelines/naming-guidelines

---
+++ Mobile/ShopLens/Droid/Activities/Camera/CameraActivity.Helpers.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+using Android.Content;
+using Android.Content.PM;
+using Android.Gms.Vision;
+using Android.Gms.Vision.Texts;
+using Android.Graphics;
+using Android.Net;
+using Android.OS;
+using Android.Provider;
+using Android.Runtime;
+using Android.Speech;
+using Camera;
+using ImageRecognitionMobile;
+using ImageRecognitionMobile.Classificators;
+using PCLAppConfig;
+using ShopLens.Droid.Helpers;
+using ShopLens.Droid.Notifications;
+using Unity;
+
+// ReSharper disable once CheckNamespace
+namespace ShopLens.Droid
+{
+    public partial class CameraActivity
+    {
+        private bool IsThereAnAppToTakePictures()
+        {
+            var intent = new Intent(MediaStore.ActionImageCapture);
+            var availableActivities =
+                PackageManager.QueryIntentActivities(intent, PackageInfoFlags.MatchDefaultOnly);
+            return availableActivities != null && availableActivities.Count > 0;
+        }
+
+        private ErrorDialogCreator GetShoppingCartAddItemDialog(RecognitionResult recognitionResult)
+        {
+            return new ErrorDialogCreator(this, Resources.GetString(Resource.String.shoppingCart),
+                Resources.GetString(Resource.String.shoppingCartQuestion),
+                Resources.GetString(Resource.String.positiveMessage),
+                Resources.GetString(Resource.String.negativeMessage),
+                () => AddToShoppingCart(recognitionResult.BestPrediction), delegate { });
+        }
+
+        private Bitmap GetResizedImageForClassification(Uri uri)
+        {
+            var maxWebClassifierImageSize = int.Parse(ConfigurationManager.AppSettings["webClassifierImgSize"]);
+            var image = MediaStore.Images.Media.GetBitmap(ContentResolver, uri);
+            image = BitmapHelper.ScaleDown(image, maxWebClassifierImageSize);
+            return image;
+        }
+
+        private static Task<Dictionary<string, float>> GetClassifyImageTask(Bitmap image)
+        {
+            var stream = new MemoryStream();
+            // 0 because compression quality is not applicable to .png
+            image.Compress(Bitmap.CompressFormat.Png, 0, stream);
+
+            var classifier = DependencyInjection.Container.Resolve<IAsyncImageClassificator>();
+            var classifyImageTask = Task.Run(
+                async () =>
+                {
+                    var results = await classifier.ClassifyImageAsync(stream.ToArray());
+                    stream.Close();
+                    return results;
+                });
+            return classifyImageTask;
+        }
+
+        private async Task<string> GetTextFromImageAsync(Bitmap image)
+        {
+            var task = Task.Run(() =>
+            {
+                var frame = new Frame.Builder().SetBitmap(image).Build();
+                var items = textRecognizer.Detect(frame);
+                var ocrResultBuilder = new StringBuilder();
+                for (var i = 0; i < items.Size(); i++)
+                {
+                    var item = (TextBlock) items.ValueAt(i);
+                    ocrResultBuilder.Append(item.Value);
+                    ocrResultBuilder.Append("/");
+                }
+
+                var ocrResult = ocrResultBuilder.ToString();
+                return ocrResult;
+            });
+
+            return await task;
+        }
+        
+        #region Unimplemented Speech Recognizer Methods
+
+        // When the user starts to speak.
+        public void OnBeginningOfSpeech() { }
+
+        // After the user stops speaking.
+        public void OnEndOfSpeech() { }
+
+        // When a network or recognition error occurs.
+        public void OnError([GeneratedEnum] SpeechRecognizerError error) { }
+
+        // When the app is ready for the user to start speaking.
+        public void OnReadyForSpeech(Bundle @params) { }
+
+        // This method is reserved for adding future events.
+        public void OnEvent(int eventType, Bundle @params) { }
+
+        // When more sound has been received.
+        public void OnBufferReceived(byte[] buffer) { }
+
+        // When the sound level of the voice input stream has changed.
+        public void OnRmsChanged(float rmsdB) { }
+
+        // When partial recognition results are available.
+        public void OnPartialResults(Bundle partialResults) { }
+
+        #endregion
+    }
+}
---
+++ Mobile/ShopLens/Droid/Activities/Camera/CameraActivity.cs
@@ -0,0 +1,288 @@
+﻿using Uri = Android.Net.Uri;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Android;
+using Android.Provider;
+using Android.Content.PM;
+using Android.App;
+using Android.Content;
+using Android.Gms.Vision.Texts;
+using Android.OS;
+using Android.Speech.Tts;
+using Android.Widget;
+using Camera;
+using Android.Support.V4.Content;
+using Android.Speech;
+using Java.Util;
+using File = Java.IO.File;
+using Android.Runtime;
+using PCLAppConfig;
+using ShopLens.Droid.Helpers;
+using Unity;
+using ShopLens.Droid.Source;
+using Android.Views;
+using ShopLens.Droid.Notifications;
+using Android.Support.Design.Widget;
+using ImageRecognitionMobile;
+using ImageRecognitionMobile.OCR;
+using ShopLens.Extensions;
+
+namespace ShopLens.Droid
+{
+
+    [Activity(Label = "CameraActivity", Theme = "@style/ShopLensTheme")]
+    public partial class CameraActivity : Activity, TextToSpeech.IOnInitListener, IRecognitionListener
+    {
+        private Button btnTakeImg;
+        private ImageView imgView;
+        private Button btnPickImg;
+        private Button recVoice;
+        private ProgressBar progressBar;
+        private CoordinatorLayout rootView;
+        private MessageBarCreator shoppingCartItemAddedMessageBar;
+        private SpeechRecognizer commandRecognizer;
+        private Intent speechIntent;
+
+        private File productPhoto;
+        private File dir =
+            new File(Android.OS.Environment.GetExternalStoragePublicDirectory(Android.OS.Environment.DirectoryPictures),
+                "DCIM");
+
+        private static readonly string FileProviderName = ConfigurationManager.AppSettings["fileProviderName"];
+        private readonly string prefsName = ConfigurationManager.AppSettings["ShopCartPrefs"];
+        
+        private static readonly int RequestImageId = (int) ActivityIds.ImageRequest;
+        private static readonly int RequestPermissionId = (int) ActivityIds.PermissionRequest;
+        private static readonly int PickImageId = (int) ActivityIds.PickImageRequest;
+        private static readonly string CmdWhatIsThis = ConfigurationManager.AppSettings["CmdWhatIsThis"];
+        private static readonly string CmdPickPhoto = ConfigurationManager.AppSettings["CmdPickPhoto"];
+
+        private IDirectoryCreator shopLensPictureDirectoryCreator;
+        private TextToSpeech tts;
+        private TextRecognizer textRecognizer;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Camera);
+
+            progressBar = FindViewById<ProgressBar>(Resource.Id.progressBar);
+            progressBar.Visibility = ViewStates.Gone;
+
+            if (IsThereAnAppToTakePictures())
+            {
+                // Set up a custom speech recognizer in this activity.
+                commandRecognizer = SpeechRecognizer.CreateSpeechRecognizer(this);
+                commandRecognizer.SetRecognitionListener(this);
+
+                shopLensPictureDirectoryCreator = DependencyInjection.Container.Resolve<IDirectoryCreator>();
+                try
+                {
+                    shopLensPictureDirectoryCreator.CreateDirectory(dir);
+                }
+                catch (Java.Lang.SecurityException e)
+                {
+                    System.Diagnostics.Debug.WriteLine(e);
+                    RequestPermissions(new string[] { Manifest.Permission.WriteExternalStorage }, RequestPermissionId);
+                }
+
+                btnTakeImg = FindViewById<Button>(Resource.Id.btntakepicture);
+                imgView = FindViewById<ImageView>(Resource.Id.ImgTakeimg);
+                rootView = FindViewById<CoordinatorLayout>(Resource.Id.root_view);
+                btnTakeImg.Click += TakeAPicture;
+
+                btnPickImg = FindViewById<Button>(Resource.Id.btnPickImage);
+                btnPickImg.Click += PickOnClick;
+
+                recVoice = FindViewById<Button>(Resource.Id.btnRecVoiceCamera);
+                recVoice.Click += RecogniseVoice;
+
+                
+                shoppingCartItemAddedMessageBar = new MessageBarCreator(rootView, Resources.GetString(Resource.String.successMessage));
+            }
+
+            tts = new TextToSpeech(this, this);
+            textRecognizer = new TextRecognizer.Builder(ApplicationContext).Build();
+        }
+
+        public void OnInit([GeneratedEnum] OperationResult status)
+        {
+            // If initialization was successful.
+            if (status == OperationResult.Success)
+            {
+                tts.SetLanguage(Locale.Us);
+            }
+            else
+            {
+                tts.SetLanguage(Locale.Default);
+            }
+        }
+
+        private void RecogniseVoice(object sender, EventArgs e)
+        {
+            speechIntent = VoiceRecognizerHelper.SetUpVoiceRecognizerIntent();
+            commandRecognizer.StartListening(speechIntent);
+        }
+
+        private void PickOnClick(object sender, EventArgs e)
+        {
+            Intent = new Intent();
+            Intent.SetType("image/*");
+            Intent.SetAction(Intent.ActionGetContent);
+            StartActivityForResult(Intent.CreateChooser(Intent, "Select Picture"), PickImageId);
+        }
+
+        private void TakeAPicture(object sender, EventArgs e)
+        {
+            var takePictureIntent = new Intent(MediaStore.ActionImageCapture);
+            productPhoto = new File(dir, $"Image_{Guid.NewGuid()}.jpg");
+            if (productPhoto != null)
+            {
+                var photoUri = FileProvider.GetUriForFile(ApplicationContext, ApplicationContext.PackageName + FileProviderName, productPhoto);
+                takePictureIntent.PutExtra(MediaStore.ExtraOutput, photoUri);
+            }
+            // If there's a working camera on the device.
+            if (takePictureIntent.ResolveActivity(PackageManager) != null)
+            {
+                StartActivityForResult(takePictureIntent, RequestImageId);
+            }
+
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            if (requestCode == RequestImageId)
+            {
+                if (resultCode == Result.Ok && productPhoto != null)
+                {
+                    var photoUri = FileProvider.GetUriForFile(ApplicationContext, ApplicationContext.PackageName + FileProviderName, productPhoto);
+                    PictureShowBitmap(photoUri);
+                    RecogniseImage(photoUri);
+                }
+            }
+            else if (requestCode == PickImageId)
+            {
+                if (resultCode == Result.Ok && data != null)
+                {
+                    PictureShowBitmap(data.Data);
+                    RecogniseImage(data.Data);
+                }
+            }
+        }
+
+        private void PictureShowBitmap(Uri uri)
+        {
+            // Put image in gallery.
+            var mediaScanIntent = new Intent(Intent.ActionMediaScannerScanFile);
+            mediaScanIntent.SetData(uri);
+            SendBroadcast(mediaScanIntent);
+
+            // Conversion. 
+            var height = imgView.Height;
+            var width = imgView.Width;
+            var image = MediaStore.Images.Media.GetBitmap(ContentResolver, uri);
+            image = BitmapHelper.ScaleDown(image, Math.Min(height, width));
+            imgView.SetImageBitmap(image);
+        }
+
+        private void RecogniseImage(Uri uri)
+        {
+            progressBar.Visibility = ViewStates.Visible;
+            Task.Run(async () =>
+                {
+                    var image = GetResizedImageForClassification(uri);
+
+                    var classifyImageTask = GetClassifyImageTask(image);
+
+                    if (!textRecognizer.IsOperational)
+                    {
+                        System.Diagnostics.Debug.WriteLine("[Warning]: Text recognizer not operational.");
+                        return new RecognitionResult { Predictions = await classifyImageTask };
+                    }
+                    
+                    var recognizeTextTask = GetTextFromImageAsync(image);
+
+                    await Task.WhenAll(classifyImageTask, recognizeTextTask);
+
+                    var ocrResult = recognizeTextTask.Result;
+                    var weightString = new RegexMetricWeightSubstringFinder().FindWeightSpecifier(ocrResult);
+
+                    var recognitionResult = new RecognitionResult
+                    {
+                        RawOcrResult = ocrResult,
+                        Predictions = classifyImageTask.Result,
+                        WeightSpecifier = weightString
+                    };
+                    return recognitionResult;
+                })
+                .ContinueWith(task =>
+                {
+                    progressBar.Visibility = ViewStates.Gone;
+                    if (task.IsFaulted)
+                    {
+                        System.Diagnostics.Debug.WriteLine(task.Exception);
+                        return;
+                    }
+
+                    var recognitionResult = task.Result;
+                    var thingsToSay = new List<string>
+                        {$"This is {recognitionResult.BestPrediction}", recognitionResult.WeightSpecifier};
+                    
+                    tts.Speak(
+                        string.Join(". ", thingsToSay),
+                        QueueMode.Flush,
+                        null,
+                        null);
+
+                    var ocrResult = recognitionResult.RawOcrResult;
+                    if (!string.IsNullOrEmpty(ocrResult))
+                        new MessageBarCreator(rootView, $"OCR: {ocrResult}").Show();
+
+                    GetShoppingCartAddItemDialog(recognitionResult).Show();
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+        }
+
+        private void AddToShoppingCart(string guess)
+        {
+            var preferences = new ActivityPreferences(this, prefsName);
+            preferences.AddString(guess.FirstCharToUpper());
+            shoppingCartItemAddedMessageBar.Show();
+        }
+
+        // When the current voice recognition session stops.
+        public void OnResults(Bundle results)
+        {
+            var matches = results.GetStringArrayList(SpeechRecognizer.ResultsRecognition);
+            if (matches.Count <= 0) return;
+            
+            if (matches[0] == CmdWhatIsThis)
+            {
+                TakeAPicture(this, new EventArgs());
+            }
+
+            else if (matches[0] == CmdPickPhoto)
+            {
+                PickOnClick(this, new EventArgs());
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            if (requestCode == RequestPermissionId)
+            {
+                if (grantResults[0] == Permission.Denied)
+                {
+                    RequestPermissions(new string[] { Manifest.Permission.WriteExternalStorage }, RequestPermissionId);
+                }
+                else
+                {
+                    shopLensPictureDirectoryCreator.CreateDirectory(dir);
+                }
+            }
+        }    
+    }
+}
+
