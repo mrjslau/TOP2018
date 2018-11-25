7b1ec6f19b8344e20409e52b1b173fd778fdcfeb
MellowJay <marijusjasinskas65@gmail.com>
2018-11-25 16:19:07 +0200

    Added SPUtteranceListener.

---
+++ Mobile/ShopLens/Droid/Helpers/ShopLensUtteranceProgressListener.cs
@@ -0,0 +1,38 @@
+﻿using Android.Speech.Tts;
+using System;
+
+namespace ShopLens.Droid.Helpers
+{
+    class ShopLensUtteranceProgressListener : UtteranceProgressListener
+    {
+        public event EventHandler<UtteranceIdArgs> OnEndOfSpeaking;
+
+        public ShopLensUtteranceProgressListener(Action<object, UtteranceIdArgs> ReactToEndOfSpeaking)
+        {
+            OnEndOfSpeaking += (obj, eargs) => ReactToEndOfSpeaking(obj, eargs);
+        }
+
+        public override void OnDone(string utteranceId)
+        {
+            OnEndOfSpeaking(this, new UtteranceIdArgs(utteranceId));
+        }
+
+        #region Unimplemented UtteranceProgressListener methods
+
+        public override void OnError(string utteranceId) { }
+
+        public override void OnStart(string utteranceId) { }
+
+        #endregion
+    }
+
+    public class UtteranceIdArgs : EventArgs
+    {
+        public string UtteranceId { get; set; }
+
+        public UtteranceIdArgs(string utteranceId)
+        {
+            UtteranceId = utteranceId;
+        }
+    }
+}
